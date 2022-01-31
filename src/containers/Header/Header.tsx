bfd213df7400e059933f0ff2e49399d7e5bc691a
Adrian <adrian@ufounders.com>
2022-01-31 08:40:47 +0100

    [FEAT]: wipe

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -3,12 +3,11 @@ import { T } from "@i18n";
 import styles from './Header.module.scss';
 
 export const Header: React.FC = () => {
-    const { total } = useDashaboard();
-    const nf = new Intl.NumberFormat('en-EN');
+    const { total, formatPrice } = useDashaboard();
     return (
         <main className={styles.container}>
             <p>{T.HEADER_BALANCE}</p>
-            <p>{`$ ${nf.format(Number(total))}`}</p>
+            <p>{`$ ${formatPrice(Number(total))}`}</p>
         </main>
     )
 }
